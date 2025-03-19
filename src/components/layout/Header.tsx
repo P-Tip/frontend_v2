6ebbb44655ae3f8a70a99c1780c6019d58346d23
gbwlxhd97 <gbwlxhd97@naver.com>
2025-03-19 23:10:37 +0900

    [feat]: svg 추가 및 logo 적용

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,11 @@
 import { NAV_LIST } from "@/constants";
 import NavItem from "./NavItem";
+import LogoIcon from "@/icons/Logo";
 
 const Header = () => {
   return (
     <header className="fixed w-full max-w-screen-lg z-10 flex justify-between items-center py-6 px-3 bg-white border-b border-gray-200">
-      <h1 className="text-xl">P-TIP</h1>
+      <LogoIcon />
       <nav className="hidden desktop:flex space-x-1.5">
         <ul className="flex space-x-4">
           {NAV_LIST.map((link) => {
---
+++ src/icons/Logo.tsx
@@ -0,0 +1,7 @@
+import Logo from "@/assets/logo.svg?react";
+
+const LogoIcon = () => {
+  return <Logo />;
+};
+
+export default LogoIcon;
