31a2101deee1d4d269f28488416d0a4f22506839
gbwlxhd97 <gbwlxhd97@naver.com>
2025-03-26 23:58:24 +0900

    [fix]: header icon 클릭시 장학금 페이지 이동

--- src/icons/Logo.tsx
+++ src/icons/Logo.tsx
@@ -1,7 +1,11 @@
 import Logo from "@/assets/logo.svg?react";
 
-const LogoIcon = () => {
-  return <Logo />;
+interface LogoIconProps {
+  onClick?: () => void;
+}
+
+const LogoIcon = ({ onClick }: LogoIconProps) => {
+  return <Logo onClick={onClick} style={{ cursor: "pointer" }} />;
 };
 
 export default LogoIcon;
