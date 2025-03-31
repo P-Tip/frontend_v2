788f5ffcf1ae2c8f0a11b71a3cf23e6d487d89a2
gbwlxhd97 <gbwlxhd97@naver.com>
2025-03-31 22:29:39 +0900

    [feat] add vite env plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import VitePluginHtmlEnv from "vite-plugin-html-env";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePluginHtmlEnv(),
+    VitePluginHtmlEnv({
+      compiler: true,
+      // compiler: false // old
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
