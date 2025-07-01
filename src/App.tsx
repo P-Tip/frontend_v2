d92e1b03682740b1cf866cadbea5a19c15f20b0f
gbwlxhd97 <gbwlxhd97@naver.com>
2025-07-01 22:49:48 +0900

    [fix]: 교내외 프로그램 페이지 디자인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./global.css";
 import Layout from "./components/layout/Layout";
 import Scholarship from "./pages/Scholarship";
-import Timetable from "./pages/Timetable";
-import Todo from "./pages/Todo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MyPage from "./pages/MyPage";
 import Home from "./pages/Home";
+import Program from "./pages/Program";
 
 function App() {
   const queryClient = new QueryClient();
@@ -19,8 +18,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/mypage" element={<MyPage />} />
             <Route path="/scholarship" element={<Scholarship />} />
-            <Route path="/timetable" element={<Timetable />} />
-            <Route path="/todo" element={<Todo />} />
+            <Route path="/program" element={<Program />} />
           </Routes>
         </Layout>
       </BrowserRouter>
